e016f970df5304d0d189b4a07714e533e2e1f751
Mintimir Kachaev <kachaev.mr@gmail.com>
2022-11-15 11:00:29 +0600

    Model for voucher

--- Domain/Entities/Applications/Application.cs
+++ Domain/Entities/Applications/Application.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿using Domain.Entities.Applications.ApplicationEnums;
+using Domain.Entities.Distances;
+using Domain.Entities.Marathons;
+using Domain.Entities.Users;
+using Domain.Entities.Vouchers;
+using System;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Domain.Entities.Applications
+namespace Domain.Entities.Applications;
+
+public class Application
 {
-    public class Application
-    {
-        public long Id { get; set; }
-        public int DistanceId { get; set; }
-        //[ForeignKey(nameof(DistanceId))]
-        //public Distance Distance { get; set; }
-    }
+    public long Id { get; set; }
+    public DateTime Date { get; set; }
+    public int Number { get; set; }
+    public bool StarterKit { get; set; }
+    public PaymentMethodEnum Payment { get; set; }
+    public int DistanceId { get; set; }
+    public int UserId { get; set; }
+    public int MarathonId { get; set; }
+    public int? PromocodeId { get; set; }
+    public Promocode? Promocode { get; set; }
+    public virtual Distance Distance { get; set; }
+    public virtual DistanceAge DistanceAge { get; set; } 
+    public virtual User User { get; set; }
+    public virtual Marathon? Marathon { get; set; }
+
 }
