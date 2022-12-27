358c6fe2dd0d4e512ddd79de843856fb0e6a3ea1
minthf <60925707+minthf@users.noreply.github.com>
2022-12-27 12:40:18 +0600

    Added semaphoreSlim to applications

--- Application/UseCases/Applications/Commands/CreateApplication/CreateApplicationCommand.cs
+++ Application/UseCases/Applications/Commands/CreateApplication/CreateApplicationCommand.cs
@@ -1,13 +1,6 @@
-﻿using Core.UseCases.Marathons.Commands.CreateMarathon;
-using Domain.Common.Contracts;
-using Domain.Entities.Applications.Exceptions;
-using Domain.Entities.Marathons;
-using Domain.Entities.Users;
+﻿using Domain.Common.Contracts;
 using Domain.Services.Interfaces;
-using Infrastructure.Services.Interfaces;
-using Mapster;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 
 namespace Core.UseCases.Applications.Commands.CraeteApplication;
@@ -24,6 +17,7 @@ public class CreateApplicationCommandHandler : IRequestHandler<CreateApplication
     private readonly IUnitOfWork _unit;
     private readonly IApplicationService _applicationService;
     private readonly IEmailService _emailService;
+    static SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
 
     public CreateApplicationCommandHandler(IUnitOfWork unit, IApplicationService applicationService, IEmailService emailService)
     {
@@ -34,37 +28,45 @@ public class CreateApplicationCommandHandler : IRequestHandler<CreateApplication
 
     public async Task<int> Handle(CreateApplicationCommand cmd, CancellationToken cancellationToken)
     {
-        var user = await _unit.UserRepository.FirstAsync(x => x.Id == cmd.UserId);
-        var distance = await _unit.DistanceRepository.FirstAsync(x => x.Id == cmd.DistanceId, include: source => source
-            .Include(a => a.Marathon)
-            .Include(a => a.DistanceAges)
-            .Include(a => a.DistancePrices)
-            .Include(a => a.Applications)
-        );
-
-        //var old_applications = _unit.ApplicationRepository.FindByCondition(predicate: x => x.User == user && x.Marathon == distance.Marathon).ToList();
-
-        //if (old_applications.Count != 0)
-        //{
-        //    throw new AlreadyRegisteredException();
-        //}
-
-
-        var marathon = distance.Marathon;
-
-        var oldStarterKitCodes = _unit.ApplicationRepository.FindByCondition(x => x.MarathonId == distance.MarathonId).Select(x => x.StarterKitCode).ToList();
-
-        if (cmd.Promocode != null)
+        await semaphore.WaitAsync();
+        try
         {
-            var promocode = await _unit.PromocodeRepository.FirstAsync(x => x.Code == cmd.Promocode && x.Distance == distance, include: source => source.Include(x => x.Voucher));
-            var application = await _applicationService.CreateApplication(user, distance, oldStarterKitCodes, promocode);
-            await _unit.ApplicationRepository.CreateAsync(application, save: true);
-            await _unit.PromocodeRepository.Update(promocode, save: true);
-            await _unit.DistanceRepository.Update(distance, save: true);
-            await _emailService.SendStarterKitCodeAsync(user.Email, application.StarterKitCode);
-            return application.Id;
-        }
+            var user = await _unit.UserRepository.FirstAsync(x => x.Id == cmd.UserId);
+            var distance = await _unit.DistanceRepository.FirstAsync(x => x.Id == cmd.DistanceId, include: source => source
+                .Include(a => a.Marathon)
+                .Include(a => a.DistanceAges)
+                .Include(a => a.DistancePrices)
+                .Include(a => a.Applications)
+            );
 
-        return -1;
+            //var old_applications = _unit.ApplicationRepository.FindByCondition(predicate: x => x.User == user && x.Marathon == distance.Marathon).ToList();
+
+            //if (old_applications.Count != 0)
+            //{
+            //    throw new AlreadyRegisteredException();
+            //}
+
+
+            var marathon = distance.Marathon;
+
+            var oldStarterKitCodes = _unit.ApplicationRepository.FindByCondition(x => x.MarathonId == distance.MarathonId).Select(x => x.StarterKitCode).ToList();
+
+            if (cmd.Promocode != null)
+            {
+                var promocode = await _unit.PromocodeRepository.FirstAsync(x => x.Code == cmd.Promocode && x.Distance == distance, include: source => source.Include(x => x.Voucher));
+                var application = await _applicationService.CreateApplication(user, distance, oldStarterKitCodes, promocode);
+                await _unit.ApplicationRepository.CreateAsync(application, save: true);
+                await _unit.PromocodeRepository.Update(promocode, save: true);
+                await _unit.DistanceRepository.Update(distance, save: true);
+                await _emailService.SendStarterKitCodeAsync(user.Email, application.StarterKitCode);
+                return application.Id;
+                }
+
+            return -1;
+        }
+        finally
+        {
+            semaphore.Release();
+        }
     }
 }
