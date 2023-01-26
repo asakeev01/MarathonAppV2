d753b8240e324bfc0f0ae50c613b6aad71e4afe7
minthf <60925707+minthf@users.noreply.github.com>
2023-01-26 14:35:09 +0600

    Removed DistanceForPWDId from Applications

---
+++ Infrastructure/Persistence/Migrations/20230126083445_RemoveDistanceForPWDId.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class RemoveDistanceForPWDId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DistanceForPWDId",
+                table: "Applications");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DistanceForPWDId",
+                table: "Applications",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
