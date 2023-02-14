8752ba108ab93cc1b8e39dc6f6bfbcbcdd776671
asakeev01 <aidar.asakeev@gmail.com>
2023-02-14 12:49:32 +0600

    receive is nullable

--- Application/UseCases/Payments/Commands/CheckPayment/CheckPaymentCommand.cs
+++ Application/UseCases/Payments/Commands/CheckPayment/CheckPaymentCommand.cs
@@ -25,30 +25,26 @@ public class CheckPaymentHandler : IRequestHandler<CheckPaymentCommand, string>
     private readonly IPaymentService _paymentService;
     private readonly ILogger<CheckPaymentHandler> _logger;
 
-    public CheckPaymentHandler(ILogger<CheckPaymentHandler> logger, IUnitOfWork unit, IApplicationService applicationService, IEmailService emailService, IPaymentService paymentService)
+    public CheckPaymentHandler(IUnitOfWork unit, IApplicationService applicationService, IEmailService emailService, IPaymentService paymentService)
     {
         _unit = unit;
         _applicationService = applicationService;
         _emailService = emailService;
         _paymentService = paymentService;
-        _logger = logger;
     }
 
     public async Task<string> Handle(CheckPaymentCommand cmd, CancellationToken cancellationToken)
     {
-        Console.WriteLine("Entered");
         var paymentRequest = cmd.PaymentDto.Adapt<CheckPaymentDto>();
         var application = await _unit.ApplicationRepository.GetFirstOrDefaultAsync(x => x.Id.ToString() == cmd.PaymentDto.pg_order_id);
         var xml = "";
         var response = new PaymentResponse();
-        Console.WriteLine(application.Id);
         if (application != null)
         {
             response.pg_description = "Payment may continue";
             response.pg_salt = "Random";
             response.pg_status = "ok";
             Console.WriteLine("Permited");
-            _logger.LogInformation("Permitted");
         }
 
         else
@@ -57,7 +53,6 @@ public class CheckPaymentHandler : IRequestHandler<CheckPaymentCommand, string>
             response.pg_salt = "Random";
             response.pg_status = "rejected";
             Console.WriteLine("Stopped");
-            _logger.LogInformation("Stopped");
         }
         response = _paymentService.CreateResponseSignature(response);
         using (var stringwriter = new Utf8StringWriter())
@@ -66,14 +61,6 @@ public class CheckPaymentHandler : IRequestHandler<CheckPaymentCommand, string>
             serializer.Serialize(stringwriter, response);
             xml = stringwriter.ToString();
         }
-        //using (var memStm = new MemoryStream())
-        //using (var xw = XmlWriter.Create(memStm))
-        //{
-        //    var serializer = new XmlSerializer(typeof(PaymentResponse));
-        //    serializer.Serialize(xw, response);
-        //    xml = memStm.ToString();
-        //}
-        Console.WriteLine(xml);
         return xml;       
     }
 }
